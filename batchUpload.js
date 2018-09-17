3ca068073ec7ad391127a73164bff1765afce350
Andi Andreas <nexuister@gmail.com>
2018-09-17 18:59:20 -0400

    Add mp4 and yt batch uploading abilities

--- batchUpload.js
+++ batchUpload.js
@@ -5,63 +5,57 @@ let dynamo = new AWS.DynamoDB({
   maxRetries: 5
 });
 
-var vids = [
-  "https://www.youtube.com/watch?v=5RsOkhR6KBc",
-  "https://www.youtube.com/watch?v=ldtpXBlh3tk",
-  "https://youtu.be/I7KeH3BDRck",
-  "https://www.youtube.com/watch?v=HaV2gOBwUPM",
-  "https://youtu.be/HM_gmycasfw",
-  "https://youtu.be/5aC4akD6RUk",
-  "https://youtu.be/yWaZ_abH5Z4",
-  "https://youtu.be/HaV2gOBwUPM",
-  "https://youtu.be/I7KeH3BDRck",
-  "https://youtu.be/GG-DkBiu_2o",
-  "https://www.youtube.com/watch?v=7GuSaJxo5S0",
-  "https://www.youtube.com/watch?v=dMA8PswEosY",
-  "https://www.youtube.com/watch?v=bSAVJHRtGz0",
-  "https://youtu.be/_S4PBMMq5yw",
-  "https://www.youtube.com/watch?v=7GuSaJxo5S0ls",
-  "https://www.youtube.com/watch?v=eAnRryw-jFY",
-  "https://www.youtube.com/watch?v=ldtpXBlh3tk",
-  "https://www.youtube.com/watch?v=YTCZYC_Ivwc",
-  "https://www.youtube.com/watch?v=7GuSaJxo5S0",
-  "https://youtu.be/I7KeH3BDRck",
-  "https://www.youtube.com/watch?v=HaV2gOBwUPM",
-  "https://youtu.be/HM_gmycasfw",
-  "https://www.youtube.com/watch?v=GG-DkBiu_2o",
-  "https://youtu.be/bLme5d8COG8",
-  "https://youtu.be/tvJJb265TaY",
-  "https://youtu.be/5z4ZTgrHGTI"
+let mp4 = [
+  "https://i.imgur.com/eVGzskW.mp4",
+  "https://i.imgur.com/fawN7bm.mp4",
+  "https://i.imgur.com/gbQp49o.mp4",
+  "https://i.imgur.com/FpDValF.mp4",
+  "https://i.imgur.com/GjZZzNE.mp4",
+  "https://i.imgur.com/XVor8TW.mp4",
+  "https://i.imgur.com/HFu3atN.mp4",
+  "https://i.imgur.com/KMCPasU.mp4",
+  "https://i.imgur.com/FouQmbK.mp4",
+  "https://i.imgur.com/XvaYgwO.mp4",
+  "https://i.imgur.com/djVzdMh.mp4",
+  "https://i.imgur.com/FGCy2rc.mp4",
+  "https://i.imgur.com/bXV8dq5.mp4",
+  "https://i.imgur.com/FASLq0h.mp4",
+  "https://video.twimg.com/ext_tw_video/1039242832798613504/pu/vid/1280x720/uM_XbG8pENWYRxVi.mp4",
+  "https://i.imgur.com/4p3Vdvf.mp4",
+  "https://i.imgur.com/SKAHoC5.mp4"
 ];
 
-let counter = 21;
+var yt = [];
+// if (vid.startsWith("https://youtu.be")) {
+//       id = vid.substring(vid.lastIndexOf("/") + 1);
+//     } else {
+//       id = vid.substring(vid.lastIndexOf("=") + 1);
+//     }
+let counter = 47;
 
 (async () => {
-  for (i in vids) {
-    let vid = vids[i];
+  for (i in mp4) {
+    let vid = mp4[i];
     let id = null;
-    if (vid.startsWith("https://youtu.be")) {
-      id = vid.substring(vid.lastIndexOf("/") + 1);
-    } else {
-      id = vid.substring(vid.lastIndexOf("=") + 1);
-    }
+
     let obj = {
       id: {
         N: `${counter}`
       },
       original: {
-        S: vid
+        S: "https://imgur.com/gallery/IBmTlIV"
       },
       src: {
-        S: `https://youtube.com/embed/${id}`
+        S: vid
       },
       submitter: {
         S: "nexuist"
       },
       type: {
-        S: "yt"
+        S: "mp4"
       }
     };
+    console.log(obj);
     await dynamo
       .putItem({
         Item: obj,
