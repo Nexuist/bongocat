e11bf229bfd51f546e5a77d41f5db21fe4de84ac
Andi Andreas <nexuister@gmail.com>
2018-09-17 11:47:43 -0400

    Add media type exclusion to endpoint

--- handler.js
+++ handler.js
@@ -1,6 +1,6 @@
 "use strict";
 const AWS = require("aws-sdk");
-const bongocats = 6;
+const bongocats = 19;
 
 // https://stackoverflow.com/a/7228322
 function randomIntFromInterval(min, max) {
@@ -13,23 +13,53 @@ let dynamo = new AWS.DynamoDB({
 });
 
 module.exports.bongo = async (event, context, callback) => {
-  let bongocat = await dynamo
-    .getItem({
-      Key: {
-        id: {
-          N: `${randomIntFromInterval(0, bongocats)}`
-        }
-      },
-      TableName: "Bongocat"
-    })
-    .promise();
+  let bongocat = null;
+  if (event.queryStringParameters && event.queryStringParameters.filter) {
+    // Filter through media types and return a random one
+    let filteredBongocats = await dynamo
+      .scan({
+        ExpressionAttributeNames: {
+          "#t": "type"
+        },
+        ExpressionAttributeValues: {
+          ":f": { S: event.queryStringParameters.filter }
+        },
+        FilterExpression: "#t <> :f",
+        TableName: "Bongocat"
+      })
+      .promise();
+    if (filteredBongocats.Count == 0) {
+      return callback(null, {
+        statusCode: 400,
+        body: "Not Found"
+      });
+    }
+    bongocat =
+      filteredBongocats.Items[
+        randomIntFromInterval(0, filteredBongocats.Count)
+      ];
+  } else {
+    // Get a random one
+    // TODO: update bongocats var automatically using DescribeTable
+    bongocat = await dynamo
+      .getItem({
+        Key: {
+          id: {
+            N: `${randomIntFromInterval(0, bongocats)}`
+          }
+        },
+        TableName: "Bongocat"
+      })
+      .promise();
+    bongocat = bongocat.Item;
+  }
   const response = {
     statusCode: 200,
     headers: {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Credentials": true
     },
-    body: JSON.stringify(bongocat.Item)
+    body: JSON.stringify(bongocat)
   };
 
   return callback(null, response);
