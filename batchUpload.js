eb07797f20f86d6b52d20a79d379184a87e1f913
Andi Andreas <nexuister@gmail.com>
2018-09-17 19:03:13 -0400

    Change autoplay warning

--- batchUpload.js
+++ batchUpload.js
@@ -5,25 +5,7 @@ let dynamo = new AWS.DynamoDB({
   maxRetries: 5
 });
 
-let mp4 = [
-  "https://i.imgur.com/eVGzskW.mp4",
-  "https://i.imgur.com/fawN7bm.mp4",
-  "https://i.imgur.com/gbQp49o.mp4",
-  "https://i.imgur.com/FpDValF.mp4",
-  "https://i.imgur.com/GjZZzNE.mp4",
-  "https://i.imgur.com/XVor8TW.mp4",
-  "https://i.imgur.com/HFu3atN.mp4",
-  "https://i.imgur.com/KMCPasU.mp4",
-  "https://i.imgur.com/FouQmbK.mp4",
-  "https://i.imgur.com/XvaYgwO.mp4",
-  "https://i.imgur.com/djVzdMh.mp4",
-  "https://i.imgur.com/FGCy2rc.mp4",
-  "https://i.imgur.com/bXV8dq5.mp4",
-  "https://i.imgur.com/FASLq0h.mp4",
-  "https://video.twimg.com/ext_tw_video/1039242832798613504/pu/vid/1280x720/uM_XbG8pENWYRxVi.mp4",
-  "https://i.imgur.com/4p3Vdvf.mp4",
-  "https://i.imgur.com/SKAHoC5.mp4"
-];
+let mp4 = [];
 
 var yt = [];
 // if (vid.startsWith("https://youtu.be")) {
